d135fe72984c5cd7a577e819e79504c23d242d47
ToastXC <100072983+toastxc@users.noreply.github.com>
2025-10-27 21:52:49 +0800

    added url paths

--- src/backend/middle.rs
+++ src/backend/middle.rs
@@ -2,48 +2,49 @@
 use crate::backend::db::DB;
 use crate::backend::{DateInfo, Habit, User};
 use dioxus::prelude::*;
-#[server]
+
+#[server(endpoint = "habits_get")]
 pub async fn habits_get(user_id: u32) -> Result<Vec<(u32, Habit)>, ServerFnError> {
     Ok(DB.habits_get_again(user_id))
 }
 
-#[server]
+#[server(endpoint = "entry_toggle")]
 pub async fn entry_toggle(habit_id: u32, day: u32) -> Result<(), ServerFnError> {
     DB.entry_toggle(habit_id, day);
     Ok(())
 }
 
-#[server]
+#[server(endpoint = "user_make")]
 pub async fn user_make() -> Result<u32, ServerFnError> {
     Ok(DB.user_make())
 }
 
-#[server]
+#[server(endpoint = "user_get")]
 pub async fn user_get(id: u32) -> Result<Option<User>, ServerFnError> {
     Ok(DB.user_get(&id))
 }
 
-#[server]
+#[server(endpoint = "habit_make")]
 pub async fn habit_make(user_id: u32, habit_name: String) -> Result<u32, ServerFnError> {
     Ok(DB.habit_make(&user_id, habit_name))
 }
 
-#[server]
+#[server(endpoint = "habit_delete")]
 pub async fn habit_delete(habit_id: u32, user_id: u32) -> Result<(), ServerFnError> {
     Ok(DB.habit_delete(&habit_id, &user_id))
 }
 
-#[server]
+#[server(endpoint = "date_get")]
 pub async fn date_get() -> Result<DateInfo, ServerFnError> {
     Ok(DB.date_get())
 }
 
-#[server]
+#[server(endpoint = "db_save")]
 pub async fn db_save() -> Result<(), ServerFnError> {
     Ok(DB.save())
 }
 
-#[server]
+#[server(endpoint = "entries_get")]
 pub async fn entries_get(habit_id: u32, entries: Vec<u32>) -> Result<Vec<bool>, ServerFnError> {
     Ok(DB.entries_get_finite(&habit_id, entries))
 }
